d27e88013d6663f7d14d36ccd5d47b64dc4f2a60
Albin Larsson <albin.post@gmail.com>
2015-07-16 23:09:52 +0200

    Basic idea
    scope of project

---
+++ src/visence.js
@@ -0,0 +1,41 @@
+/*
+
+visence.init('element-id');
+*/
+
+var visence = {
+  init: function(id, settings) {
+    var defaultstrings {
+      c00 [
+        'cc0',
+        'cc 0',
+        'cc-0',
+        'Zero (0)'
+      ],
+      ccby [
+        'ccby',
+        'cc by',
+        'cc-by',
+        'Attribution (BY)'
+      ],
+      ccsa [
+        'ccsa',
+        'cc sa',
+        'cc-sa',
+        'Share-alike (SA)'
+      ],
+      ccnc [
+        'ccnc',
+        'cc nc',
+        'cc-nc',
+        'Non-commercial (NC)'
+      ],
+      ccnd [
+        'ccnc',
+        'cc nc',
+        'cc-nc',
+        'No Derivative Works (ND)'
+      ]
+    };
+  }
+};
