8fbc7e2f49eb19e6bae7666c26db5131f5f93c01
Albin Larsson <albin.post@gmail.com>
2015-07-17 19:40:35 +0200

    First working

--- src/visence.js
+++ src/visence.js
@@ -1,41 +1,71 @@
 /*
-
 visence.init('element-id');
 */
 
 var visence = {
-  init: function(id, settings) {
-    var defaultstrings {
-      c00 [
-        'cc0',
-        'cc 0',
-        'cc-0',
-        'Zero (0)'
-      ],
-      ccby [
-        'ccby',
-        'cc by',
-        'cc-by',
-        'Attribution (BY)'
-      ],
-      ccsa [
-        'ccsa',
-        'cc sa',
-        'cc-sa',
-        'Share-alike (SA)'
-      ],
-      ccnc [
-        'ccnc',
-        'cc nc',
-        'cc-nc',
-        'Non-commercial (NC)'
-      ],
-      ccnd [
-        'ccnc',
-        'cc nc',
-        'cc-nc',
-        'No Derivative Works (ND)'
-      ]
+  init: function(id) {
+    var licenses = {
+      c00: {
+        link: '#',
+        text: 'text',
+        strings: [
+          'cc0',
+          'cc 0',
+          'cc-0',
+          'Zero (0)'
+        ]
+      },
+      ccby: {
+        link: '#',
+        text: 'text',
+        strings: [
+          'ccby',
+          'cc by',
+          'cc-by',
+          'Attribution (BY)'
+        ]
+      },
+      ccsa: {
+        link: '#',
+        text: 'text',
+        strings: [
+          'ccsa',
+          'cc sa',
+          'cc-sa',
+          'Share-alike (SA)'
+        ]
+      },
+      ccnc: {
+        link: '#',
+        text: 'text',
+        strings: [
+          'ccnc',
+          'cc nc',
+          'cc-nc',
+          'Non-commercial (NC)'
+        ]
+      },
+      ccnd: {
+        link: '#',
+        text: 'text',
+        strings: [
+          'ccnc',
+          'cc nc',
+          'cc-nc',
+          'No Derivative Works (ND)'
+        ]
+      }
+    };
+
+    for (var license in licenses) {
+      var keyStrings = licenses[license]['strings'];
+      for (var string in keyStrings) {
+        var newLicenseString = '<a href="' + licenses[license]['link'] + '">' + licenses[license]['text'] + '</a>';
+        var targetString = document.getElementById(id).innerHTML;
+        if (targetString.indexOf(licenses[license]['strings'][string]) != -1) {
+          document.getElementById(id).innerHTML = targetString.split(licenses[license]['strings'][string]).join(newLicenseString);
+        };
+      };
     };
   }
 };
